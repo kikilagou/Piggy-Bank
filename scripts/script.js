36e8da5ac4d95d531e696296103a47b55f4f4f0e
Kiki Lagou <kiklagou@publicissapient.com>
2019-10-23 16:18:28 +0100

    Set up bare bones for javascript

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// DATA MODULE
+var dataController = (function() {
+
+});
+
+// UI MODULE
+var UIController = (function() {
+
+});
+
+// CONTROLLER MODULE
+var controller = (function(budgetCtrl, UICtrl) {
+
+});
