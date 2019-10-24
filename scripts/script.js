915c27f9d114c66e32685d287b1e45332baa9415
Kiki Lagou <kiklagou@publicissapient.com>
2019-10-24 13:16:56 +0100

    Added functionality to add items to our data controller

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,57 @@
 // DATA MODULE
 var dataController = (function () {
 
-    var Expense = function(id, description, value) {
+    var Expense = function(id, name, description, amount) {
         this.id = id;
+        this.name = name;
         this.description = description;
-        this.value = value;
+        this.amount = amount;
     }
 
-    var Income = function(id, description, value) {
+    var Income = function(id, name, description, amount) {
         this.id = id;
+        this.name = name
         this.description = description;
-        this.value = value;
+        this.amount = amount;
     }
 
     data = {
         allItems: {
-            inc: [],
-            exp: []
+            '+': [],
+            '-': []
         }, 
         totals: {
-            inc: 0,
-            exp: 0
+            '+': 0,
+            '-': 0
         }
     }
 
+    // Allow other functions to add items 
+    return {
+        addItem: function(type, name, desc, amount) {
+            var newItem, ID;
+
+            if(data.allItems[type].length > 0 ){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+             
+            if (type === '+') {
+                newItem = new Income(ID, name, desc, amount);
+            } else if (type === '-') {
+                newItem = new Expense(ID, name, desc, amount);
+            }
+
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data); 
+        }
+    }
 
 
 })();
@@ -66,7 +94,7 @@ var UIController = (function () {
 })();
 
 // CONTROLLER MODULE
-var controller = (function (budgetCtrl, UICtrl) {
+var controller = (function (dataCtrl, UICtrl) {
 
     var setUpEventListeners = function () {
         var DOM = UICtrl.getDOMStrings();
@@ -80,16 +108,20 @@ var controller = (function (budgetCtrl, UICtrl) {
     }
 
     var ctrlAddItem = function (type) {
+        var input, newItem;
+
         // get input data from fields
         if (type === "+") {
-            var input = UICtrl.getIncomeInput()
+            input = UICtrl.getIncomeInput()
             console.log(input)
         } else if (type === '-') {
-            var input = UICtrl.getExpenseInput();
+            input = UICtrl.getExpenseInput();
             console.log(input)
         }
 
         // add item to data contorller
+        newItem = dataCtrl.addItem(input.type, input.name, input.description, input.amount); 
+
         // add new item to user interface 
         // calc budget
         // display budget 
